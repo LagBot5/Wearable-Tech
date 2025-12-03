ce2108ce26883745383e535b27b161ff1b0f3fca
Gruuben33 <ruubenpuddle@gmail.com>
2025-12-03 11:35:15 -0500

    Enhance sensor data output and adjust sleep timing
    Refactor printData function to include rotation and speed for both MPU sensors, and adjust sleep duration in the main loop.

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ turnedOff = True
 modeCycleButton = Pin(15, Pin.IN, Pin.PULL_DOWN)
 turnedOffA = True
 
+now = ticks_ms()
+lastTimeCheck1 = ticks_ms()
+millisecondsSinceLastTimeCheck1 = now - lastTimeCheck1
+lastTimeCheck2 = ticks_ms()
+millisecondsSinceLastTimeCheck2 = now - lastTimeCheck2
+
 #Sets of I2C
 I2C1 = I2C(0, sda=Pin(0), scl=Pin(1))
 I2C2 =  I2C(1, sda=Pin(2), scl=Pin(3))
@@ -26,16 +32,35 @@ gestures = 2
 mode = 0
 
 def printData():
-    print("1 X =", gyroscope1.x)
-    print("1 Y =", gyroscope1.y)
-    print("2 X =", gyroscope2.x)
-    print("2 Y =", gyroscope2.y)
-    print("1 temp=",imu1.temperature)
-    print("2 temp=",imu2.temperature)
+    # MPU 1
+    print("1 X Rotation =", gyroscope1.x)
+    print("1 Y Rotation =", gyroscope1.y)
+    print("1 Z Rotation =", gyroscope1.z)
+    print("1 X Speed =", accel1.x)
+    print("1 Y Speed =", accel1.y)
+    print("1 Z Speed =", accel1.z)
+    print("1 temp =",imu1.temperature)
+    # MPU 2
+    print("2 X Rotaion =", gyroscope2.x)
+    print("2 Y Rotaion =", gyroscope2.y)
+    print("2 Z Rotaion =", gyroscope2.z)
+    print("2 X Speed =", accel2.x)
+    print("2 Y Speed =", accel2.y)
+    print("2 Z Speed =", accel2.z)
+    print("2 temp =",imu2.temperature)
+    sleep(1)
 
 
 
 while True:
+    note_playing = False
+
+    # reading values
+    gyroscope1 = imu1.gyro
+    accel1 = imu1.accel
+    gyroscope2 = imu2.gyro
+    accel2 = imu2.accel
+
     # short term mode cycling button
     if modeCycleButton.value() == 1:
         if turnedOffA == True:
@@ -46,28 +71,7 @@ while True:
     elif modeCycleButton.value() == 0:
         turnedOffA = True
 
-    note_playing = False
-    
-    # reading values
-    gyroscope1 = imu1.gyro
-    accel1 = imu1.accel
-    gyroscope2 = imu2.gyro
-    accel2 = imu2.accel
-    
-    #Turning On Button
-#    if button.value() == 1:
-#        if state == 0:
-#            print("Turn On!")
-#            state = 1
-#            StateLed.value(1)
-#            sleep(1)
-#        elif state == 1:
-#            print("Turn Off!")
-#            state = 0
-#            StateLed.value(0)
-#            sleep(1)
-#    elif state == 0:
-#        StateLed.value(0)
+    # Debounced power button
     if button.value() == 1:
         if turnedOff == True:
             turnedOff = False
@@ -81,14 +85,13 @@ while True:
                 StateLed.value(1)
     elif button.value() == 0:
         turnedOff = True
-        
 
+    #If power on, do this
     if state == 1:
         #printData() # <-- working function
         FingerLogic.checkFingers(gyroscope1, accel1, gyroscope2, accel2, mode)
-        #print(accel1.x, accel1.y, accel1.z)
 
         #Stops Note Playing
         if not note_playing:  # If no button is pressed
             Tones.bequiet()
-    sleep(0.001)
+    sleep(0.1)
